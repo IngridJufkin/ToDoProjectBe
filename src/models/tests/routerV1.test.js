a250cb2347cf17dd823a73f1253d8fab6465a316
Ingrid Jufkin <Ingrid@Ingrids-MBP.lan>
2021-05-26 13:55:33 +0300

    Backend tests added

--- src/models/tests/routerV1.test.js
+++ src/models/tests/routerV1.test.js
@@ -1,4 +1,4 @@
-const { getAllTasks, createTask, deleteTask } = require('../../controllers');
+const { getAllTasks, createTask, deleteTask, getTodoTasks, getDoneTasks, getAllUsers, moveTask, downloadFile, deleteDoneTask, deleteTodoTask, getTasksByName} = require('../../controllers');
 
 const spy = jest.fn(); // spy loob kunstlikult routeri
 //const getSpy = jest.fn();
@@ -34,6 +34,43 @@ describe('router', () => { //kasutas seda ruuterit
   //All router number
   test('should test ALL tasks', () => {
     expect(spy).toHaveBeenCalledTimes(11);
+  });
+
+  test('should test todo tasks', () => {
+    expect(spy).toHaveBeenNthCalledWith(2, '/todo-tasks', getTodoTasks);
+  });
+
+  test('should test done tasks', () => {
+    expect(spy).toHaveBeenNthCalledWith(3, '/done-tasks', getDoneTasks);
+  });
+
+  test('should test All Useers', () => {
+    expect(spy).toHaveBeenNthCalledWith(4, '/all-users', getAllUsers); 
+  });
+
+  test('should test move tasks', () => {
+    expect(spy).toHaveBeenNthCalledWith(5, '/moveTask/:id/:toTask', moveTask);
+  });
+
+  test('should test download file', () => {
+    expect(spy).toHaveBeenNthCalledWith(7, "/downloadFile", downloadFile);
+  });
+
+  test('should test Done Tasks', () => {
+    expect(spy).toHaveBeenNthCalledWith(8,"/:id/:toTask", deleteDoneTask);
+  });
+
+  test('should test delete Todo tasks', () => {
+    expect(spy).toHaveBeenNthCalledWith(9,"/:id/:toTask", deleteTodoTask);
+  });
+
+
+  test('should test delete Tasks', () => {
+    expect(spy).toHaveBeenNthCalledWith(10,"/deleteTask", deleteTask);
+  });
+
+  test('should test getTasks By Name with Username', () => {
+    expect(spy).toHaveBeenNthCalledWith(11,"/getTasksByName/:userName", getTasksByName);
   });
 
  //Only router GET tasks 
